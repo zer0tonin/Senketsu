9bbfb50c6a3c263961e1b374034bec61dda920ce
AliceGG <alice.girard@protonmail.com>
2020-05-30 14:22:41 +0200

    Trying out oak

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
-import { serve } from 'https://deno.land/std/http/server.ts';
+import { Application, Router, Context } from "https://deno.land/x/oak/mod.ts";
 
-const server = serve({ port: 8080 });
-console.log("starting server on localhost:8080")
-for await (const req of server) {
-  req.respond({ body: "Hello World\n" });
-}
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello World!";
+  })
+  .get("/:string", (context) => {
+    context.response.body = "Hello " + context.params.string;
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Starting server on port 8080");
+await app.listen({ port: 8080 });
