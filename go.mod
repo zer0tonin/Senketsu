eca6a88e38ea6383bb193df3eed1fc58a9d96987
AliceGG <alice.girard@protonmail.com>
2020-09-06 20:18:59 +0200

    S3 Upload

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.14
 
-require github.com/spf13/viper v1.7.1 // indirect
+require (
+	github.com/minio/minio-go/v7 v7.0.5 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -7,26 +7,54 @@ import (
 	"mime/multipart"
 	"net/http"
 
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/spf13/viper"
 )
 
-func config() {
+type Services struct {
+	S3Client  *minio.Client
+	Templates map[string]*template.Template
+}
+
+var s Services
+
+func init() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(fmt.Sprintf("Failed to load config"))
-	} else {
-		fmt.Println(viper.GetString("test"))
+		panic("Failed to load config")
+	}
+
+	minioClient, err := minio.New(
+		viper.GetString("s3.endpoint"),
+		&minio.Options{
+			Creds: credentials.NewStaticV4(
+				viper.GetString("s3.accessKeyID"),
+				viper.GetString("s3.secretAccessKey"),
+				"",
+			),
+			Secure: viper.GetBool("s3.useSSL"),
+		},
+	)
+	if err != nil {
+		panic("Failed to create S3 client")
+	}
+
+	templates := make(map[string]*template.Template)
+	templates["index"] = template.Must(template.ParseFiles("./templates/index.html"))
+
+	s = Services{
+		S3Client:  minioClient,
+		Templates: templates,
 	}
 }
 
-func serve() {
-	tmpl := template.Must(template.ParseFiles("./templates/index.html"))
-
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		s.Templates["index"].Execute(w, nil)
 	})
 
 	http.HandleFunc("/index.css", func(w http.ResponseWriter, r *http.Request) {
@@ -34,25 +62,47 @@ func serve() {
 	})
 
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
 		_, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		fmt.Println("Parsed MediaType")
+
 		mr := multipart.NewReader(r.Body, params["boundary"])
-		part, err := mr.NextPart()
+		form, err := mr.ReadForm(134217728) // 128MiB
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(part.FileName())
+		fmt.Println("Parsed Multipart Form")
+
+		for _, fileHeaders := range form.File {
+			for _, fileHeader := range fileHeaders {
+				file, err := fileHeader.Open()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				_, err = s.S3Client.PutObject(
+					ctx,
+					"senketsu-test",
+					fileHeader.Filename,
+					file,
+					fileHeader.Size,
+					minio.PutObjectOptions{},
+				)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Printf("Put Object %s to S3\n", fileHeader.Filename)
+				}
+			}
+		}
 	})
 
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
-}
-
-func main() {
-	config()
-	serve()
 }
